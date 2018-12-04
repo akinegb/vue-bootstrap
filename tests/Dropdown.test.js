5cd14790d3265536d052f9c2932a7db6a4f15aa1
akin.egbwole <akin.egbwole@fjordnet.com>
2018-12-04 16:09:41 +0000

    Dropdown test(s)

---
+++ tests/Dropdown.test.js
@@ -0,0 +1,22 @@
+import { mount, createLocalVue } from "@vue/test-utils";
+import { render } from "@vue/server-test-utils"
+import Vuex from "vuex";
+import Dropdown from "../src/components/Dropdown.vue";
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe("Dropdown select widget", () => {
+  let propData;
+  beforeEach(() => {
+    propData = {
+      options: []
+    }
+  });
+
+  it("mounted as expected", () => {
+    const wrapper = mount(Dropdown, { propData });
+    expect(wrapper.findAll(".dropdown-group").length).toEqual(1)
+  });
+});
