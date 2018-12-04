aeb0fa2128357f2ca3326e9818da3a10ad081b3a
akin.egbwole <akin.egbwole@fjordnet.com>
2018-12-04 17:13:26 +0000

    Base API service test(s)

---
+++ tests/BaseAPIService.test.js
@@ -0,0 +1,18 @@
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import { render } from "@vue/server-test-utils"
+
+describe("Base API", () => {
+  beforeEach(() => {
+    // store = new Vuex.Store({
+    //   state: {}
+    // })
+  });
+
+  it("makes call to endpoint", () => {
+
+  });
+
+  it("get JSON response", () => {
+
+  });
+});
