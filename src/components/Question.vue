51ddf9355289bbf8177ac5986170472d0726a287
akin.egbwole <akin.egbwole@fjordnet.com>
2018-12-06 18:15:30 +0000

    Created question component

---
+++ src/components/Question.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="question">
+    <p>Question <span>{{step}}</span></p>
+    <slot/>
+    <Button
+      :click="backStep"
+      name="Back"/>
+    <Button
+      :click="nextStep"
+      name="Next Step"/>
+  </div>
+</template>
+<script>
+import Button from "./Button"
+export default {
+  name: "Question",
+  props: ["title", "QuestionNumber", "step", "nextStep", "backStep"],
+  components: { Button },
+}
+</script>
+<style lang="scss">
+  .question {
+    display: block;
+    text-align: left;
+    font-size: 1.25em;
+  }
+</style>
